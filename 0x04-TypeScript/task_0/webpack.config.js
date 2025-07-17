cd5efecce3f95d3a58e8bdeb60811f85c295f276
Shyaka Ndamukunda <shyakandamukunda@gmail.com>
2025-07-17 13:04:33 +0200

    Update webpack.config.js

--- 0x04-TypeScript/task_0/webpack.config.js
+++ 0x04-TypeScript/task_0/webpack.config.js
@@ -1,1 +1,26 @@
+const path = require('path');
 
+module.exports = {
+  entry: './js/main.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '/'),
+    },
+  },
+};
